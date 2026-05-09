37212729614451b0222918483447a1ff8eb28806
Hiroaki Iwamori <morizo0505@gmail.com>
2026-05-10 00:08:26 +0900

    update: comment追加

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,11 @@
 import { defineConfig } from 'vite';
 
+/*
+IIFE とは Immediately Invoked Function Expression の略で、関数を定義して即時実行することを指す。
+この設定では、client.ts をビルドして client.js を出力する際に、関数を即時実行するようにしている。
+つまり、ブラウザでそのまま動く形式で出力するようにしている。
+ */
+
 export default defineConfig({
   build: {
     outDir: 'dist',
